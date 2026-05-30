93edb421c9f715e07186869c8e1f110c68cffd1a
minhquan-tool <voquan28027@gmail.com>
2026-05-30 09:07:03 +0700

    Update __init__.py

--- Project_btvn/bai_tap_ve_nha/modun/__init__.py
+++ Project_btvn/bai_tap_ve_nha/modun/__init__.py
@@ -6,7 +6,12 @@ from .dfs_optimal import DFS_OPTIMAL
 from .ucs import UCS
 from .ids import IDS
 from .ids_early import IDS_EARLY
-from .greedy_algorithm import GreedyAlgorithm
-from .a_star import AStar
+from .greedy_algorithm import GREEDYALGORITHM
+from .a_star import A_STAR
+from .ida_star import IDA_STAR
+from .simple_hill_climbing import SIMPLE_HC
+from .ignorant_hill_climbing import IGNORANT_HC
+from .random_hill_climbing import RANDOM_HC
 __all__ = ['BaseAlg', 'BFS', 'BFS_FAST', 'DFS', 'DFS_OPTIMAL', 'UCS', 'IDS', 
-           'IDS_EARLY', 'GreedyAlgorithm', 'AStar']
+           'IDS_EARLY', 'GREEDYALGORITHM', 'A_STAR', 'IDA_STAR', 'SIMPLE_HC', 
+           'IGNORANT_HC', 'RANDOM_HC']
